e011d0ad6eca286d739a3a23feff506c4b460dd3
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-07-09 23:37:56 +0800

    Add function to give direct message

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -27,9 +27,22 @@ class MicroBlogger
       case command
         when 'q' then puts "Goodbye!"
         when 't' then tweet(parts[1..-1].join(" "))
+        when 'dm' then dm(parts[1], parts[2..-1].join(" "))
         else
           puts "Sorry, I don't know how to #{command}"
       end
+    end
+  end
+
+  def dm(target, message)
+    puts "Trying to send #{target} this direct message:"
+    puts message
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+    if screen_names.include?(target)
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "Error: You can only DM people who follow you"
     end
   end
 end
